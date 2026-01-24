bc061d5bfc1b71be676986e0abd654f78652be83
Beton <dima41768@icloud.com>
2026-01-24 17:51:16 +0300

    feat: add UI model for cart item
    - UICartItem with id, image, title, rating, price properties

---
+++ FakeNFT/Scenes /Cart/Models/UICartItem.swift
@@ -0,0 +1,16 @@
+//
+//  UICartItem.swift
+//  FakeNFT
+//
+//  Created by Дмитрий Чалов on 24.01.2026.
+//
+
+import UIKit
+
+struct UICartItem {
+    let id: UUID
+    let image: UIImage
+    let title: String
+    let rating: String
+    let price: String
+}
