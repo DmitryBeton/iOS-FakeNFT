3bd27c30eb9c21e846d5a408dd8f8b1380176e8d
Beton <dima41768@icloud.com>
2026-01-31 12:10:31 +0300

    feat(cart): Add UICurrency model with fullName, shortName and logo properties

---
+++ FakeNFT/Scenes /Cart/Models/UICurrency.swift
@@ -0,0 +1,14 @@
+//
+//  UICurrency.swift
+//  FakeNFT
+//
+//  Created by Дмитрий Чалов on 31.01.2026.
+//
+
+import UIKit
+
+struct UICurrency {
+    let fullName: String
+    let shortName: String
+    let logo: UIImage
+}
