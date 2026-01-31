0240b1685a3c2904da9f6265b3d116a377db8f2e
BeerHobbit <kovalenko537@gmail.com>
2026-01-31 21:04:55 +0300

    feat(profile): implement profile service api methods

--- FakeNFT/Scenes /Profile/Services/ProfileService.swift
+++ FakeNFT/Scenes /Profile/Services/ProfileService.swift
@@ -2,22 +2,57 @@ import Foundation
 
 typealias ProfileCompletion = (Result<Profile, Error>) -> Void
 
-final class ProfileService {
+final class ProfileService: ProfileServiceProtocol {
     
-    private let networkClient: NetworkClient
-    private let storage: ProfileStorage
-    
-    init(networkClient: NetworkClient, storage: ProfileStorage) {
-        self.networkClient = networkClient
-        self.storage = storage
-    }
+    // MARK: - Public Methods
     
     func loadProfile(completion: @escaping ProfileCompletion) {
+        if let profile = storage.getProfile() {
+            completion(.success(profile))
+            return
+        }
         
+        let request = LoadProfileRequest()
+        networkClient.send(
+            request: request,
+            type: Profile.self
+        ) { [weak storage] result in
+            switch result {
+            case .success(let profile):
+                storage?.saveProfile(profile)
+                completion(.success(profile))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
     }
     
-    func updateProfile(with profile: ProfileDto, completion: @escaping ProfileCompletion) {
-        
+    func updateProfile(with profileDto: ProfileDto, completion: @escaping ProfileCompletion) {
+        let request = UpdateProfileRequest(dto: profileDto)
+        networkClient.send(
+            request: request,
+            type: Profile.self
+        ) { [weak storage] result in
+            switch result {
+            case .success(let profile):
+                storage?.saveProfile(profile)
+                completion(.success(profile))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    // MARK: - Private Properties
+    
+    private let networkClient: NetworkClient
+    private let storage: ProfileStorageProtocol
+    
+    // MARK: - Init
+    
+    init(networkClient: NetworkClient, storage: ProfileStorageProtocol) {
+        self.networkClient = networkClient
+        self.storage = storage
     }
     
 }
--- FakeNFT/Scenes /Profile/Services/ProfileStorage.swift
+++ FakeNFT/Scenes /Profile/Services/ProfileStorage.swift
@@ -1,5 +1,24 @@
-final class ProfileStorage {
+import Foundation
+
+final class ProfileStorage: ProfileStorageProtocol {
     
+    // MARK: - Public Methods
     
+    func saveProfile(_ profile: Profile) {
+        syncQueue.async { [weak self] in
+            self?.storage = profile
+        }
+    }
+    
+    func getProfile() -> Profile? {
+        syncQueue.sync {
+            storage
+        }
+    }
+    
+    // MARK: - Private Properties
+    
+    private var storage: Profile?
+    private let syncQueue = DispatchQueue(label: "sync-profile-queue")
     
 }
