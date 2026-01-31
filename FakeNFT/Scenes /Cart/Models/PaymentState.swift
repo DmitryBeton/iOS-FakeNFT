d1acc9ff91fc0ad1fb1782c8e0c9acf577f8637f
Beton <dima41768@icloud.com>
2026-01-31 12:11:48 +0300

    feat(cart): Add PaymentState enum with cases: selecting, confirming, success, error

---
+++ FakeNFT/Scenes /Cart/Models/PaymentState.swift
@@ -0,0 +1,13 @@
+//
+//  PaymentState.swift
+//  FakeNFT
+//
+//  Created by Дмитрий Чалов on 31.01.2026.
+//
+
+enum PaymentState {
+    case selecting
+    case confirming
+    case success
+    case error
+}
