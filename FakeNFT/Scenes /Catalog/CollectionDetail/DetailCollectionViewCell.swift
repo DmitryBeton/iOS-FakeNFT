87465318e06fa7a5cde0b34771d873391d170553
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2026-02-03 12:04:47 +0300

    add imageView for cell

--- FakeNFT/Scenes /Catalog/CollectionDetail/DetailCollectionViewCell.swift
+++ FakeNFT/Scenes /Catalog/CollectionDetail/DetailCollectionViewCell.swift
@@ -5,6 +5,16 @@ final class DetailCollectionViewCell: UICollectionViewCell {
     // MARK: - Properties
     static let reuseIdentifier = "DetailCollectionViewCell"
     
+    // MARK: - UI Elements
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 12
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
     // MARK: - Initialization
     override init(frame: CGRect) {
         super.init(frame: frame)
