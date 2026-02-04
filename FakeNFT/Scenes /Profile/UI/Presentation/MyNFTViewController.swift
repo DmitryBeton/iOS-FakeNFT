9a3342269346f0dbd2af1e451e91f91460c48283
BeerHobbit <kovalenko537@gmail.com>
2026-02-04 16:40:52 +0300

    feat(myNFT): add sort enum

--- FakeNFT/Scenes /Profile/UI/Presentation/MyNFTViewController.swift
+++ FakeNFT/Scenes /Profile/UI/Presentation/MyNFTViewController.swift
@@ -2,6 +2,12 @@ import UIKit
 
 final class MyNFTViewController: UIViewController {
     
+    // MARK: - Private Types
     
+    private enum SortOption {
+        case price
+        case rating
+        case name
+    }
     
 }
