1e72e1bcdc85fd6748ef16a0f169e558bbebcf38
BeerHobbit <kovalenko537@gmail.com>
2026-02-09 22:00:14 +0300

    feat(myNFT): add thread-safe storage for nfts

--- FakeNFT/Scenes /Profile/Services/ProfileNftStorage.swift
+++ FakeNFT/Scenes /Profile/Services/ProfileNftStorage.swift
@@ -1,7 +1,32 @@
+import Foundation
+
 protocol ProfileNftStorageProtocol {
-    
+    func saveNfts(_ nfts: [ProfileNft])
+    func getNfts() -> [ProfileNft]
 }
 
 final class ProfileNftStorage: ProfileNftStorageProtocol {
+    
+    // MARK: - Public Methods
+    
+    func saveNfts(_ nfts: [ProfileNft]) {
+        concurrentQueue.async(flags: .barrier) {
+            self.storage = nfts
+        }
+    }
+    
+    func getNfts() -> [ProfileNft] {
+        concurrentQueue.sync {
+            storage
+        }
+    }
+    
+    // MARK: - Private Properties
+    
+    private var storage: [ProfileNft] = []
+    private let concurrentQueue = DispatchQueue(
+        label: "profile-nft-storage-queue",
+        attributes: .concurrent
+    )
     
 }
