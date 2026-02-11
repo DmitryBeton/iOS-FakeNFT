506c07093f4ffb0fb1a3fa36a489f7a6a1091e08
BeerHobbit <kovalenko537@gmail.com>
2026-02-11 05:47:57 +0300

    feat(favourites): add favourite cell layout

--- FakeNFT/Scenes /Profile/UI/Presentation/FavouritesViewController.swift
+++ FakeNFT/Scenes /Profile/UI/Presentation/FavouritesViewController.swift
@@ -2,11 +2,18 @@ import UIKit
 
 final class FavouritesViewController: UIViewController, NetworkErrorView {
     
+    // MARK: - Private Types
+    
+    private enum Section {
+        case main
+    }
+    
     // MARK: - Views
     
     private lazy var favouritesCollectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(FavouriteCell.self)
         return collectionView
     }()
     
@@ -19,6 +26,56 @@ final class FavouritesViewController: UIViewController, NetworkErrorView {
         return label
     }()
     
+    // MARK: - DiffableDataSource
+    
+    private lazy var dataSource: UICollectionViewDiffableDataSource<Section, FavouriteNftUI> = {
+        let dataSource = UICollectionViewDiffableDataSource<Section, FavouriteNftUI>(
+            collectionView: favouritesCollectionView
+        ) { [weak self] collectionView, indexPath, nft in
+            let cell: FavouriteCell = collectionView.dequeueReusableCell(indexPath: indexPath)
+            cell.configure(nft: nft)
+            return cell
+        }
+        return dataSource
+    }()
+    
+    // MARK: - Private Properties
+    
+    private let mockNfts: [FavouriteNftUI] = [
+        FavouriteNftUI(
+            name: "commodo porttitor",
+            image: URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Beige/April/1.png"),
+            rating: 3,
+            price: "36.54",
+            id: UUID(),
+            isLiked: true
+        ),
+        FavouriteNftUI(
+            name: "commodo porttitor",
+            image: URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Beige/April/1.png"),
+            rating: 3,
+            price: "36.54",
+            id: UUID(),
+            isLiked: true
+        ),
+        FavouriteNftUI(
+            name: "commodo porttitor",
+            image: URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Beige/April/1.png"),
+            rating: 3,
+            price: "36.54",
+            id: UUID(),
+            isLiked: false
+        ),
+        FavouriteNftUI(
+            name: "commodo porttitor",
+            image: URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Beige/April/1.png"),
+            rating: 3,
+            price: "36.54",
+            id: UUID(),
+            isLiked: false
+        ),
+    ]
+    
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
@@ -26,6 +83,7 @@ final class FavouritesViewController: UIViewController, NetworkErrorView {
         setupViews()
         setupNavigationBar()
         setupConstraints()
+        applySnapshot(nfts: mockNfts, animating: false)
     }
     
     // MARK: - UI Methods
@@ -53,6 +111,15 @@ final class FavouritesViewController: UIViewController, NetworkErrorView {
         ])
         
         emptyLabel.constraintCenters(to: view)
+    }
+    
+    // MARK: - Private Methods
+    
+    private func applySnapshot(nfts: [FavouriteNftUI], animating: Bool) {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, FavouriteNftUI>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(nfts, toSection: .main)
+        dataSource.apply(snapshot, animatingDifferences: animating)
     }
     
 }
